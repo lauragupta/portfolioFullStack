f637b56ef86303d96f276fcf5e5f33a719b39691
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-02-18 21:52:25 -0800

    add index.js for models

---
+++ models/index.js
@@ -0,0 +1,15 @@
+const Skill = require('./Skill');
+const Project = require('./Project');
+const Employment = require('./Employment');
+
+Project.hasMany(Skill, {
+  foreignKey: 'project_id',
+  onDelete: 'CASCADE'
+});
+
+Employment.hasMany(Skill, {
+  foreignKey: 'employment_id',
+  onDelete: 'CASCADE'
+});
+
+module.exports = { Skill, Project, Employment };
